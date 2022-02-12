3ba30a8a52bb4f3947befb4c95f4d4c397871540
Dane-LeBlanc <dane.leblanc@gmail.com>
2022-02-11 22:31:13 -0700

    Included local storage features for exercise list (when creating new workout). Can also clear all and remove individual exercises

--- static/addWorkoutExercises.js
+++ static/addWorkoutExercises.js
@@ -2,28 +2,61 @@
 const $container = $(".container-workout");
 const BASE_URL = "http://127.0.0.1:5000/api";
 const $selectedList = $("#selected-exercises");
-
+const $clearBtn = $("#clear-button");
+let excList = [];
+// Creating an AMRAP
 if (window.location.href.indexOf("/create/AMRAP/") != -1) {
-  const excList = [];
-  let $excPerStage = +$("#exc-per-stage").val();
-  setStages();
+  let $stagesVal = +$("#stages").val();
+  let $excPerStageVal = +$("#exc-per-stage").val();
+  if (localStorage.excList) {
+    for (let ex of JSON.parse(localStorage.excList)) {
+      excList.push(ex);
+    }
+    setStages();
+    fillStages();
+  } else {
+    setStages();
+  }
+
   $("#stages").change(function () {
     $selectedList.empty();
+    $stagesVal = +$("#stages").val();
     setStages();
+    fillStages();
   });
-  $container.on("click", ".btn-success", async function (e) {
+  $("#exc-per-stage").change(function () {
+    $selectedList.empty();
+    $excPerStageVal = +$("#exc-per-stage").val();
+    setStages();
+    fillStages();
+  });
+  $container.on("click", ".btn-success", function (e) {
     e.preventDefault();
     let exerciseId = +$(e.target).attr("data-id");
-    excList.push(exerciseId);
-    let exercise = await axios.get(`${BASE_URL}/exercises/${exerciseId}`);
-    let exerciseName = exercise.data.exercise.name;
-    let $added_exercise = $(
-      `<li data-id = ${exerciseId}> ${exerciseName}</li>`
+    let exerciseName = $(e.target).attr("data-name");
+    let newExc = { id: exerciseId, name: exerciseName };
+    if (excList.length <= $stagesVal * $excPerStageVal) {
+      excList.push(newExc);
+      localStorage.clear();
+      localStorage.setItem("excList", JSON.stringify(excList));
+    }
+    let $addedExercise = $(
+      `<li class = "li-exercise" data-id = ${exerciseId}> ${exerciseName}</li>`
     );
-    console.log(exerciseId);
-    console.log($excPerStage);
-    if (excList.indexOf(exerciseId) < $excPerStage) {
-      $added_exercise.appendTo($(".stage-1"));
+    let currStage = Math.floor((excList.length - 1) / $excPerStageVal) + 1;
+    $addedExercise.appendTo(`.stage-${currStage}`);
+  });
+  $container.on("dblclick", ".li-exercise", function (e) {
+    let excId = +$(e.target).attr("data-id");
+    for (let ex of excList) {
+      if (ex.id === excId) {
+        let index = excList.indexOf(ex);
+        excList.splice(index, 1);
+        localStorage.setItem("excList", JSON.stringify(excList));
+        $(e.target).remove();
+        location.reload();
+        return;
+      }
     }
   });
   function setStages() {
@@ -33,4 +66,23 @@ if (window.location.href.indexOf("/create/AMRAP/") != -1) {
       $(`<ul>`).attr("class", `stage-${i}`).appendTo($selectedList);
     }
   }
+  function fillStages() {
+    let index = 0;
+    for (let exc of excList) {
+      let $newLi = $(
+        `<li class = "li-exercise" data-id = ${exc.id}> ${exc.name}</li>`
+      );
+      let currStage = Math.floor(index / $excPerStageVal) + 1;
+      $newLi.appendTo(`.stage-${currStage}`);
+      index = index + 1;
+    }
+  }
+  $clearBtn.on("click", function () {
+    localStorage.clear();
+    location.reload();
+  });
+  $("nav").on("click", "a", function () {
+    console.log("clicked");
+    localStorage.clear();
+  });
 }
